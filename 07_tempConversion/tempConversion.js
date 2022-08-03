94c5fec68f3bd84c44e4b313c661900160b98227
John Gibson <jhngbsn09@gmail.com>
2022-08-03 11:08:01 -0400

    Completed the exersizes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+    let newVar = (far - 32) * (5/9)
+    return Math.round(newVar * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+    let newVar = cel * (9/5) + 32
+    return Math.round(newVar * 10) / 10
 };
+
+//npm test tempConversion.spec.js
 
 // Do not edit below this line
 module.exports = {
