626870c01d26b967418eea12e1636b8d3df17b2a
John Gibson <jhngbsn09@gmail.com>
2022-08-03 16:23:26 -0400

    Completed calculator lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let num = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    num += arr[i];
+  }
+
+  return num;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let num = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = num * arr[i];
+  }
+
+  return num;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	num = Math.pow(a, b);
+  return num;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0){
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num -1))
+  }
 };
 
 // Do not edit below this line
